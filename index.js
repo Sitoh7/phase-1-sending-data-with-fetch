d1a6602d810881d6d8d14f75a0c3f8f6685def8f
Sitoh7 <ha.wakhule@gmail.com>
2024-10-08 11:44:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(userName,userEmail){
+    
+    return fetch(`http://localhost:3000/users`,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(resp => document.body.innerHTML = resp.id)
+    .catch(error=>  document.body.innerHTML= error)
+
+    
+}
